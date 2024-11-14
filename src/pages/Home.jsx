14b0df1fe5c788cb38e61048bdd171d42638e89c
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-14 22:15:16 +0530

    Added:- Subscribe Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/Hero/Hero";
 import Products from "../components/Products/Products";
 import TopProducts from "../components/TopProducts/TopProducts";
 import Banner from "../components/Banner/Banner";
+import Subscribe from "../components/Subscribe/Subscribe";
 const Home = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
@@ -12,6 +13,7 @@ const Home = () => {
       <Products />
       <TopProducts />
       <Banner />
+      <Subscribe />
     </div>
   )
 }
