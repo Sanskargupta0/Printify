92e91ede7abdf49fc04f54dc5092ad9b4a37fefc
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-14 19:06:38 +0530

    Added:- Products Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { pages } from './pages';
 
 function App() {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
       <BrowserRouter>
         <Routes>
